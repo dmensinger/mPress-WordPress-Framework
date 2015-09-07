cf569bff17770f8c09b3295d2ae7a57b92fccd1f
dmensinger <dan@mpresscreative.com>
2015-09-07 15:05:19 -0400

    Added social links

--- footer.php
+++ footer.php
@@ -3,6 +3,14 @@
     <div id="nav-footer"><?php wp_nav_menu('menu=Main Menu'); ?></div>
 
     Copyright &copy; <?php echo date("Y"); ?> Your Business. All Rights Reserved<br /><br />
+                   
+    <div id="smedia">
+        <a class="fb" href="#" target="_blank"></a>
+        <a class="tw" href="#" target="_blank"></a>
+        <a class="gp" href="#" target="_blank"></a>
+        <a class="yt" href="#" target="_blank"></a>
+        <a class="in" href="#" target="_blank"></a>
+    </div>
 
 </div>
 	
