e3fe57d639f32c62c474040fc0442248052fe9e7
dmensinger <dan@mpresscreative.com>
2015-09-07 21:34:02 -0400

    Tidy up code

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,4 @@
 <div id="sidebar">
-	<?php 	/* Widgetized sidebar, if you have the plugin installed. */
-            if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('General Sidebar') ) : ?>
+	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('General Sidebar') ) : ?>
     <?php endif; ?>
 </div>
