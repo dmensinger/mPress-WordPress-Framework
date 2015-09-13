f4372cb9c502d91ef98c5bd9a746e4f439407e34
dmensinger <dan@mpresscreative.com>
2015-09-13 12:01:51 -0400

    Minor changes

--- home.php
+++ home.php
@@ -10,23 +10,17 @@ Template Name: Home
 
 <div id="page">
 	<div class="wrapper">
-    
-        <div class="two_third full_width_md">
-            <div id="content">
-    
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    
-                <div class="entry">
-                    <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-                </div>          
-                
-            <?php endwhile; endif; ?>
-           
-            </div>
-        </div>
-	
-        <div class="one_third full_width_md">
-            <?php get_sidebar(); ?>
+
+        <div id="content">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <div class="entry">
+                <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+            </div>          
+            
+        <?php endwhile; endif; ?>
+       
         </div>
     
     </div>
